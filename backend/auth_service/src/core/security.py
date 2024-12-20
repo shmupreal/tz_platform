dfdcc2d4c54321a2c1d96075f95f93c61154a274
seregaziguli <seregaziguli@gmail.com>
2024-12-20 16:07:45 +0300

    deleted security function

--- backend/auth_service/src/core/security.py
+++ backend/auth_service/src/core/security.py
@@ -15,25 +15,6 @@ def hash_password(password):
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-def is_password_strong_enough(password: str) -> bool:
-    """if len(password < 5):
-        return False
-    
-    if not any(char.isupper() for char in password):
-        return False
-    
-    if not any(char.islower() for char in password):
-        return False
-    
-    if not any(char.isdigit() for char in password):
-        return False
-    
-    if not any(char in SPECIAL_CHARACTERS for char in password):
-        return False"""
-    
-    return True
-
-
 def str_encode(string: str) -> str:
     return base64.b85encode(string.encode('ascii')).decode('ascii')
 
